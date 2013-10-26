21d56d271239a955a2f4c5bcc2ce31da6fd70896
andrey.sendme@gmail.com <andrey.sendme@gmail.com@97555c3f-ed5f-4e7e-2ca0-bf85a361f195>
2013-10-26 02:15:12 +0000

    Debug times

--- trunk/Magic/src/com/detection/Hand.java
+++ trunk/Magic/src/com/detection/Hand.java
@@ -1,17 +1,28 @@
 package com.detection;
 
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 
 public class Hand {
 
+	private final Rect rect;
 	private final Point center;
 	private final boolean open;
 	
+	public Hand(Rect rect, boolean open){
+		this.rect = rect;
+		this.center = HandDetect.centerOf(rect);
+		this.open = open;
+	}
 	
 	public Hand(Point center, boolean open){
+		this.rect = null;
 		this.center = center;
 		this.open = open;
-		
+	}
+	
+	public Rect getRect(){
+		return rect;
 	}
 	
 	public Point getCenter(){
--- trunk/Magic/src/com/detection/HandDetect.java
+++ trunk/Magic/src/com/detection/HandDetect.java
@@ -5,6 +5,8 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.*;
 import org.opencv.android.CameraBridgeViewBase.*;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,32 +20,35 @@ public class HandDetect {
 	}
 	
 	private static List<Hand> findHands(Mat src){
-		
 		ArrayList<Hand> foo = new ArrayList<Hand>();
 
 		Imgproc.GaussianBlur(src,  src, new Size(5,5), 1);
-	    Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2GRAY);
 	    
 	    src.convertTo(src, CvType.CV_8U);
 	    MatOfRect fistLocations = new MatOfRect();
 	    MatOfRect palmLocations = new MatOfRect();
 	    
+	    
 	    fistClassifier.detectMultiScale(src, fistLocations);
 	    palmClassifier.detectMultiScale(src, palmLocations);
 	    
+	    
 	    for (Rect rect : fistLocations.toArray()) {
-	    	foo.add(new Hand( centerOf(rect), false));
+	    	foo.add(new Hand( rect, false));
 	    }
 	    
 	    for( Rect rect : palmLocations.toArray()) {
-	    	foo.add(new Hand(centerOf(rect), true));
+	    	foo.add(new Hand(rect, true));
 	    }
 	    
 		return foo;
 		
 	}
 	
-	private static Point centerOf(Rect rect){
+	
+	
+	public static Point centerOf(Rect rect){
 		return new Point(rect.x + rect.width/2, rect.y+rect.height );
 	}
 	
